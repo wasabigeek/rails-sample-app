27f2f8541e582dc9970bc817723587088086a00d
Nicholas Koh <nkohwl@gmail.com>
2018-07-03 23:08:05 +0800

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,6 +16,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
   end
 
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: {
+        name: "Example User",
+        email: "user@valid.com",
+        password: "password",
+        password_confirmation: "password"
+      }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
   test 'correct post url' do
     get signup_path
     assert_select 'form[action=?]', signup_path
