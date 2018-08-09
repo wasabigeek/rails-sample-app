9c3c4530d25fa13ca186eaca48b537a8e2d5b74c
Nicholas Koh <nkohwl@gmail.com>
2018-08-09 14:19:18 +0800

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :activation_token
 
-    before_save { email.downcase! }
+    before_save :downcase_email
+    before_create :create_activation_digest
+
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\w\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
@@ -22,18 +24,38 @@ class User < ApplicationRecord
         SecureRandom.urlsafe_base64
     end
 
+    def activate
+        update_attributes(activated: true, activated_at: Time.zone.now)
+    end
+
+    # returns true if given token matches digest
+    def authenticated?(attribute, token)
+        digest = self.send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(token)
+    end
+
     def remember
         self.remember_token = User.new_token
         update_attribute(:remember_digest, User.digest(remember_token))
     end
 
-    # returns true if given token matches digest
-    def authenticated?(remember_token)
-        return false if remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    def send_activation_email
+        UserMailer.account_activation(self).deliver_now
     end
 
     def forget
         update_attribute(:remember_digest, nil)
     end
+
+    private
+
+        def downcase_email
+            self.email.downcase!
+        end
+
+        def create_activation_digest
+            self.activation_token = User.new_token
+            self.activation_digest = User.digest(activation_token)
+        end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
@@ -26,6 +30,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         password_confirmation: "password"
       }}
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    # Try to log in before activation
+    log_in_as(user)
+    assert_not is_logged_in?
+    # Invalid activation token
+    get edit_account_activation_path(user.activation_token, email: "wrong")
+    assert_not is_logged_in?
+    # Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
